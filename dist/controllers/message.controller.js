0f83629a5ca929c17983f3291bf7d120617877db
dhruv15803 <Dhruvshetty960@gmail.com>
2024-04-26 16:39:04 +0530

    added conversation model and new controllers

--- dist/controllers/message.controller.js
+++ dist/controllers/message.controller.js
@@ -1,8 +1,10 @@
 import { Message } from '../models/message.model.js';
+import { Conversation } from '../models/conversation.model.js';
 const createMessage = async (req, res) => {
     try {
         // need to be logged in to create/send a message
         // protected route
+        let newConversation;
         const { senderId, receiverId, message } = req.body;
         if (senderId !== req.userId) {
             res.status(400).json({
@@ -19,9 +21,44 @@ const createMessage = async (req, res) => {
             return;
         }
         const newMessage = await Message.create({ senderId, receiverId, message });
+        // if there is no conversation between senderId and receiverId then create one and put message into it
+        // else put this message in the conversation that already exists
+        let conversation = await Conversation.findOne({ participants: { $all: [senderId, receiverId] } });
+        if (!conversation) {
+            conversation = await Conversation.create({ participants: [senderId, receiverId], messages: [newMessage._id] });
+        }
+        else {
+            conversation.messages.push(newMessage._id);
+            await conversation.save();
+        }
         res.status(201).json({
             'success': true,
             "message": "sent message successfully",
+            "sentMessage": newMessage,
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+const getConversation = async (req, res) => {
+    // protected route
+    try {
+        const { receiverId } = req.params;
+        const senderId = req.userId;
+        // get messages between sender and receiver from conversation
+        const conversation = await Conversation.findOne({ participants: { $all: [senderId, receiverId] } }).populate('messages');
+        if (!conversation) {
+            res.json({
+                "success": false,
+                "message": "send a message to start a conversation",
+            });
+            return;
+        }
+        const messages = conversation?.messages;
+        res.status(200).json({
+            "success": true,
+            "conversationMessages": messages,
         });
     }
     catch (error) {
@@ -42,4 +79,18 @@ const getMessagesFromSenderId = async (req, res) => {
         console.log(error);
     }
 };
-export { createMessage, getMessagesFromSenderId };
+const getMessagesFromLoggedInUser = async (req, res) => {
+    try {
+        const { receiverId } = req.params;
+        const senderId = req.userId;
+        const messages = await Message.find({ senderId, receiverId });
+        res.status(200).json({
+            "success": true,
+            messages,
+        });
+    }
+    catch (error) {
+        console.log(error);
+    }
+};
+export { createMessage, getMessagesFromSenderId, getMessagesFromLoggedInUser, getConversation, };
--- dist/routes/message.routes.js
+++ dist/routes/message.routes.js
@@ -1,7 +1,9 @@
 import express from 'express';
-import { createMessage, getMessagesFromSenderId } from '../controllers/message.controller.js';
+import { createMessage, getConversation, getMessagesFromLoggedInUser, getMessagesFromSenderId } from '../controllers/message.controller.js';
 import { protectRoute } from '../middlewares/user.middleware.js';
 const router = express.Router();
 router.post('/create', protectRoute, createMessage);
 router.get('/getMessages/:senderId', protectRoute, getMessagesFromSenderId);
+router.get('/getMessagesLoggedIn/:receiverId', protectRoute, getMessagesFromLoggedInUser);
+router.get('/getConversation/:receiverId', protectRoute, getConversation);
 export default router;
