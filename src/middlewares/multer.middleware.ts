90e6e739b0c2156c407721870be15a8981ac884f
dhruv15803 <Dhruvshetty960@gmail.com>
2024-05-01 20:00:33 +0530

    img upload deployment fix

--- src/middlewares/multer.middleware.ts
+++ src/middlewares/multer.middleware.ts
@@ -2,7 +2,7 @@ import multer from "multer";
 
 const storage = multer.diskStorage({
     destination: function (req, file, cb) {
-      cb(null, './src/Public')
+      cb(null, './dist/Public')
     },
     filename: function (req, file, cb) {
       cb(null,file.originalname)
